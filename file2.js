b1f75d9153146dff64a61b07add86838a054e84b
Inderjit Singh Rai <744004@student.tor.loc>
2019-04-04 11:35:21 -0400

    done

--- file2.js
+++ file2.js
@@ -1,10 +1,12 @@
 var user={
-    first_name:"CSS",
-    last_name:"HTML",
-    age:4,/* from www.java2s.com
-    website:"java2s.com"
-
-};
-for(key in user){
+    first_Name: "CSS",
+    last_Name: "HTML",
+    age: 4,/*from w w w.java 2 s . o m*/
+    website: "java2s.com"
+    };
+    
+    for (key in user){
     console.log(key);
-}
+    }
+    
+    
